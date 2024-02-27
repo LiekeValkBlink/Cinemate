e03bb1667eb12894966bc2b639067416cc9aaea7
LiekeValkBlink <lieke.valk@blink.nl>
2024-02-27 11:55:46 +0100

    Add different layouts for Admin and User

--- Cinemate.API/obj/Debug/net8.0/Cinemate.API.AssemblyInfo.cs
+++ Cinemate.API/obj/Debug/net8.0/Cinemate.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinemate.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042e38be27e8398dfb8e5a1fa4e39cc5527df03f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa2b34e347de9436c924cc68921cae295a7d882")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinemate.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinemate.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
