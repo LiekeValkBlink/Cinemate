11be06427768bafefab19e812a5415303f31aade
Corina Bakker <corbadesign@gmail.com>
2024-03-02 23:49:50 +0100

    Take two

--- Cinemate.Web/Program.cs
+++ Cinemate.Web/Program.cs
@@ -4,6 +4,9 @@ using Cinemate.Web;
 using Cinemate.Web.Services;
 using Cinemate.Web.Services.Contracts;
 using Radzen;
+using Syncfusion.Blazor;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NAaF1cXmhKYVFzWmFZfVpgcV9FaFZTRGYuP1ZhSXxXdkdhUX9cc3dUQWZZWEE=");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazorBootstrap();
 builder.Services.AddRadzenComponents();
 builder.Services.AddBlazorBootstrap();
+builder.Services.AddSyncfusionBlazor();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7201/") });
 builder.Services.AddScoped<IMovieService, MovieService>();
