3053def43aadfd8970335ea3722aee1110c9faf0
Corina Bakker <corbadesign@gmail.com>
2024-03-03 10:55:57 +0100

    Almost final layout

--- Cinemate.Models/Dto/BarcodeDto.cs
+++ Cinemate.Models/Dto/BarcodeDto.cs
@@ -10,7 +10,7 @@ namespace Cinemate.Models.Dto
     {
         public int Id { get; set; }
         public string? MovieName { get; set; }
-        public DateOnly Date { get; set; }
+        public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
         public TimeOnly Time { get; set; }
         public int Room { get; set; }
 
