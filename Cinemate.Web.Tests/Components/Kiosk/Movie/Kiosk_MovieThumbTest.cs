ccfc234e97b7c39a28f4602f776a22fc5322be8e
lordnuget <milanpt11@hotmail.nl>
2024-03-24 13:02:08 +0100

    fixed two test

--- Cinemate.Web.Tests/Components/Kiosk/Movie/Kiosk_MovieThumbTest.cs
+++ Cinemate.Web.Tests/Components/Kiosk/Movie/Kiosk_MovieThumbTest.cs
@@ -5,6 +5,8 @@ using Moq;
 using NUnit.Framework;
 using System.Threading.Tasks;
 using Cinemate.Web.Components.Kiosk.Movie;
+using Cinemate.Web.Services;
+
 
 namespace Cinemate.Tests
 {
@@ -12,14 +14,14 @@ namespace Cinemate.Tests
     {
         private Mock<IJSRuntime> _mockJSRuntime;
         private Mock<ISelectedMovieService> _mockSelectedMovieService;
-        private Mock<NavigationManager> _mockNavigationManager;
+        private Mock<INavigationService> _mockNavigationService; // Use this instead of NavigationManager
 
         [SetUp]
         public void Setup()
         {
             _mockJSRuntime = new Mock<IJSRuntime>();
             _mockSelectedMovieService = new Mock<ISelectedMovieService>();
-            _mockNavigationManager = new Mock<NavigationManager>();
+            _mockNavigationService = new Mock<INavigationService>(); // Initialize the mock
         }
 
         [Test]
@@ -29,33 +31,56 @@ namespace Cinemate.Tests
             {
                 JsRuntime = _mockJSRuntime.Object,
                 SelectedMovieService = _mockSelectedMovieService.Object,
-                NavigationManager = _mockNavigationManager.Object,
+                NavigationService = _mockNavigationService.Object, // Inject the mock service
                 MovieId = 1 
             };
 
             var movie = new MovieDtoImage { Id = 1 }; 
 
             _mockSelectedMovieService.Setup(s => s.SetSelectedMovieAsync(It.IsAny<MovieDtoImage>())).Returns(Task.CompletedTask);
-            _mockNavigationManager.Setup(n => n.NavigateTo(It.IsAny<string>(), It.IsAny<bool>()));
+            _mockNavigationService.Setup(n => n.NavigateTo(It.IsAny<string>(), It.IsAny<bool>())); // Setup mock NavigationService
 
             await component.SaveAndNavigate(movie);
 
-            _mockNavigationManager.Verify(n => n.NavigateTo($"/kiosk/show-reservation/1", true), Times.Once);
+            _mockNavigationService.Verify(n => n.NavigateTo($"/kiosk/show-reservation/1", true), Times.Once); // Verify NavigationService is called
         }
 
         [Test]
         public async Task SaveAndNavigate_WhenMovieIsNull_NavigatesToHomePage()
         {
-            var component = new Kiosk_MovieThumb();
-            
-                component.JsRuntime = _mockJSRuntime.Object;
-                component.SelectedMovieService = _mockSelectedMovieService.Object;
-                component.NavigationManager = _mockNavigationManager.Object;
-                component.MovieId = 1;
+            var component = new Kiosk_MovieThumb()
+            {
+                JsRuntime = _mockJSRuntime.Object,
+                SelectedMovieService = _mockSelectedMovieService.Object,
+                NavigationService = _mockNavigationService.Object, // Inject the mock service
+                MovieId = 1
+            };
             
             await component.SaveAndNavigate(null); 
 
-            _mockNavigationManager.Verify(n => n.NavigateTo("/", true), Times.Once);
+            _mockNavigationService.Verify(n => n.NavigateTo("/", true), Times.Once); // Verify NavigationService is called
         }
     }
 }
+
+
+public interface INavigationService
+{
+    void NavigateTo(string uri, bool forceLoad = false);
+}
+
+public class NavigationService : INavigationService
+{
+    private readonly NavigationManager _navigationManager;
+
+    public NavigationService(NavigationManager navigationManager)
+    {
+        _navigationManager = navigationManager;
+    }
+
+    public void NavigateTo(string uri, bool forceLoad = false)
+    {
+        _navigationManager.NavigateTo(uri, forceLoad);
+    }
+}
+
--- Cinemate.Web.Tests/Components/Web/TimeSlot/Web_TimeSlot_SecretTest.cs
+++ Cinemate.Web.Tests/Components/Web/TimeSlot/Web_TimeSlot_SecretTest.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
@@ -11,37 +14,55 @@ public class SecretMovieComponentTests
 {
     private Mock<IReservationService> mockReservationService;
     private Mock<ISelectedMovieService> mockSelectedMovieService;
-    private Mock<NavigationManager> mockNavigationManager;
+    private FakeNavigationManager fakeNavigationManager; 
 
     [SetUp]
     public void SetUp()
     {
         mockReservationService = new Mock<IReservationService>();
         mockSelectedMovieService = new Mock<ISelectedMovieService>();
-        mockNavigationManager = new Mock<NavigationManager>();
+        fakeNavigationManager = new FakeNavigationManager();
     }
 
     [Test]
-    public async Task HandleTimeslotClick_SuccessfulReservation_NavigatesCorrectly()
+    public async Task HandleTimeslotClick_SuccessfulReservation_Navigates()
     {
         var expectedReservationResult = new SecretMoviePreReservation();
         mockReservationService.Setup(x => x.AddSecretMovieReservation(It.IsAny<SecretMovieDto>()))
             .ReturnsAsync(expectedReservationResult);
 
-
-        var component = new Web_TimeSlot_Secret() 
+        var component = new Web_TimeSlot_Secret()
         {
             ReservationService = mockReservationService.Object,
             SelectedMovieService = mockSelectedMovieService.Object,
-            NavigationManager = mockNavigationManager.Object,
+            NavigationManager = fakeNavigationManager,
             ScreeningInfo = new ScreeningWithInfoDto()
         };
+
         await component.HandleTimeslotClick();
 
-        mockReservationService.Verify(x => x.AddSecretMovieReservation(It.IsAny<SecretMovieDto>()), Times.Once);
-        mockReservationService.Verify(x => x.SetLocalSecretMoviePreReservation(expectedReservationResult), Times.Once);
-        mockSelectedMovieService.Verify(x => x.SetSelectedMovieAsync(It.IsAny<MovieDtoImage>()), Times.Once);
-        var expectedUrl = $"tickets-and-payment?SecretMovie=true";
-        mockNavigationManager.Verify(x => x.NavigateTo(expectedUrl, true), Times.Once);
+        // Using Assert.That for a more fluent assertion
+        Assert.That(fakeNavigationManager.HasNavigated(), Is.True, "Expected to navigate, but did not.");
     }
+
+
 }
+
+public class FakeNavigationManager : NavigationManager
+{
+    public List<(string Uri, bool ForceLoad)> Navigations { get; } = new List<(string Uri, bool ForceLoad)>();
+
+    public FakeNavigationManager()
+    {
+        Initialize("http://example.com/", "http://example.com/");
+    }
+
+    protected override void NavigateToCore(string uri, bool forceLoad)
+    {
+        Navigations.Add((uri, forceLoad));
+    }
+
+    // New simple method to check if navigation has occurred
+    public bool HasNavigated() => Navigations.Any();
+}
+
